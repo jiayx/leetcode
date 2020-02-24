96b167b0b97248158fd62543d1a335fe53f97bc6
jiayx <i@jiayx.net>
2020-02-24 20:51:06 +0800

    简化逻辑 减少内存占用

--- decodeways.go
+++ decodeways.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	s := "12343520342344553"
-	i := decode(s)
+	// i := decode(s)
+	i := decode1(s)
 	fmt.Println(i)
 }
 
@@ -16,18 +17,31 @@ func decode(s string) int {
 	length := len(s)
 	arr := make([]int, length+1)
 	arr[0] = 1
-	if s[0] == '0' {
-		arr[1] = 0
-	} else {
-		arr[1] = 1
-	}
+	arr[1] = between(string(s[0]))
 
 	i := 2
 	for i <= length {
 		arr[i] = arr[i-1]*between(s[i-1:i]) + arr[i-2]*between(s[i-2:i])
-		i = i + 1
+		i++
 	}
 	return arr[length]
+}
+
+// 简化为用两个变量存值
+func decode1(s string) int {
+	length := len(s)
+
+	prev := 1
+	curr := between(string(s[0]))
+
+	i := 2
+	for i <= length {
+		temp := curr
+		curr = curr*between(s[i-1:i]) + prev*between(s[i-2:i])
+		prev = temp
+		i++
+	}
+	return curr
 }
 
 func between(s string) int {
